57675163dd6a5d2391fbfd04d11dae4440894aa5
lukas <lukas.westermann@gmail.com>
2009-10-08 13:00:35 +0200

    test fallback to CGI#escape if Rack::Utils is not available (by pro-activly undefing Rack::Utils)

---
+++ test/gravatarify_rack_vs_cgi_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+require 'gravatarify/base'
+
+class GravatarifyRackVsCgiTest < Test::Unit::TestCase
+  include Gravatarify::Base
+  
+  # Reload Rack::Utils
+  def teardown
+    begin; require('rack/utils'); rescue LoadError; end
+  end
+
+  context "if Rack::Utils is not available, #gravatarify" do
+    setup do
+      # Remove Rack if defined
+      Object.send(:remove_const, :Rack) if defined?(Rack)
+    end
+    
+    should "fallback to CGI#escape" do
+      assert !defined?(Rack::Utils), 'Rack::Utils should no longer be defined'
+      assert defined?(CGI), "CGI should be defined"
+      assert_equal "http://0.gravatar.com/avatar/1cacf1bc403efca2e7a58bcfa9574e4d.jpg?escaped%2Fme=escaped%2Fme",
+                   build_gravatar_url('bella@gmail.com', 'escaped/me' => 'escaped/me')
+    end
+  end
+end
