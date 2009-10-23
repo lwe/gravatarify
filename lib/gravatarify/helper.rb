86e7d15443612d06f55a0122af7d8fe3c984ad67
lukas <lukas.westermann@gmail.com>
2009-10-23 16:47:48 +0200

    yeah, better implementation of reject/delete_if-combo using inject

--- lib/gravatarify/helper.rb
+++ lib/gravatarify/helper.rb
@@ -12,10 +12,9 @@ module Gravatarify::Helper
   #    %img{ gravatar_attrs(@user.mail, :size => 20) }/
   #
   def gravatar_attrs(email, options = {})
-    url_options = options.reject { |key, value| !Gravatarify::GRAVATAR_OPTIONS.include?(key) }
-    options = options.delete_if { |key, value| Gravatarify::GRAVATAR_OPTIONS.include?(key) }
+    url_options = options.inject({}) { |hsh, (key, value)| hsh[key] = options.delete(key) if Gravatarify::GRAVATAR_OPTIONS.include?(key.to_sym); hsh }
     options[:alt] ||= Gravatarify::Base.get_smart_email_from(email) # use email as :alt attribute
-    options[:width] = options[:height] = (url_options[:size] || Gravatarify::GRAVATAR_DEFAULT_SIZE) # customize size
+    options[:width] = options[:height] = (url_options[:size] || 80) # customize size
     options[:src] = email.respond_to?(:gravatar_url) ? email.gravatar_url(url_options) : build_gravatar_url(email, url_options)
     options
   end
