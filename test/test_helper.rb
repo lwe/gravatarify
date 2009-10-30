5b1f5a0e512aea637d401c1430988871c29c3511
lukas <lukas.westermann@gmail.com>
2009-10-30 08:45:46 +0100

    removed html options

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,10 +13,10 @@ def reset_gravatarify!
   Gravatarify.options[:filetype] = :jpg
   Gravatarify.subdomains = %w{ 0 1 2 www }
   
-  if defined?(Gravatarify::Helper)
-    Gravatarify::Helper.html_options.clear
-    Gravatarify::Helper.html_options[:alt] = ''
-  end
+  #if defined?(Gravatarify::Helper)
+  #  Gravatarify::Helper.html_options.clear
+  #  Gravatarify::Helper.html_options[:alt] = ''
+  #end
 end
 
 # some often used values...
