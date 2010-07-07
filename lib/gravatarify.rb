6abd059c7720ecf7bb6f03427fed2338b8d11fe7
lukas <lukas.westermann@gmail.com>
2010-07-07 10:23:03 +0200

    added fix for rails 3 support, so helpers are loaded automatically

--- lib/gravatarify.rb
+++ lib/gravatarify.rb
@@ -1,12 +1,13 @@
 # Loads all required submodules
 module Gravatarify
   # current API version, as defined by http://semver.org/
-  VERSION = "2.1.0".freeze
+  VERSION = "2.2.0".freeze
   
   autoload :Base,   'gravatarify/base'
   autoload :Utils,  'gravatarify/utils'
   autoload :Helper, 'gravatarify/helper'
 end
 
-# and add HAML support (if defined)
+# hook into HAML and ActionView
 Haml::Helpers.send(:include, Gravatarify::Helper) if defined?(Haml)
+ActionView::Base.send(:include, Gravatarify::Helper) if defined?(ActionView)
--- rails/init.rb
+++ rails/init.rb
@@ -1,4 +1,1 @@
 require 'gravatarify'
-
-# include view helpers only if ActionView is available
-ActionView::Base.send(:include, Gravatarify::Helper) if defined?(ActionView)
