bfd64d088f5a68f621fd9ac02d0d9e99661b037c
lukas <lukas.westermann@gmail.com>
2010-08-30 15:54:00 +0200

    removed autoloads

--- lib/gravatarify.rb
+++ lib/gravatarify.rb
@@ -1,19 +1,10 @@
-# Loads all required submodules
+require 'gravatarify/base'
+require 'gravatarify/utils'
+require 'gravatarify/helper'
+
 module Gravatarify
   # current API version, as defined by http://semver.org/
   VERSION = "2.2.0".freeze
-  
-  autoload :Base,   'gravatarify/base'
-  autoload :Utils,  'gravatarify/utils'
-  autoload :Helper, 'gravatarify/helper'
-  
-  def self.setup
-    Rails.logger.error "[Gravatarify#setup] HAML=#{defined?(Haml) ? 'yes' : 'no'}, ActionView=#{defined?(ActionView) ? 'yes' : 'no'}"
-        
-    # try to hook into HAML and ActionView
-    Haml::Helpers.send(:include, Gravatarify::Helper) if defined?(Haml)
-    ActionView::Base.send(:include, Gravatarify::Helper) if defined?(ActionView)    
-  end
 end
 
 if defined?(ActiveSupport) && ActiveSupport.responds_to?(:on_load)
