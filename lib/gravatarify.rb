e573407be621c0ee62b39631bc25721e9ab0d538
lukas <lukas.westermann@gmail.com>
2010-11-15 10:02:47 +0100

    include into HAML, even when ActiveSupport is present (fix for issue#1)

--- lib/gravatarify.rb
+++ lib/gravatarify.rb
@@ -10,8 +10,9 @@ end
 if defined?(ActiveSupport) && ActiveSupport.respond_to?(:on_load)
   # Support for rails 3
   ActiveSupport.on_load(:action_view) { include Gravatarify::Helper }
-else
-  # try to hook into HAML and ActionView
-  Haml::Helpers.send(:include, Gravatarify::Helper) if defined?(Haml)
+elsif defined?(ActionView)
+  # hook into rails 2.x
   ActionView::Base.send(:include, Gravatarify::Helper) if defined?(ActionView)
 end
+# try to include it in HAML too, no matter what :)
+Haml::Helpers.send(:include, Gravatarify::Helper) if defined?(Haml)
